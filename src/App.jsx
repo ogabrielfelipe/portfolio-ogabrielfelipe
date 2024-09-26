254136ae0e20ec259733a1fa29013ab530236b29
Gabriel Felipe Vieira <ogabriel.v.felipe@gmail.com>
2024-09-26 10:37:25 -0300

    feat: implements details of the projects and texts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { Cpu } from "lucide-react";
-import { Github } from "lucide-react";
 import { ArrowBigUp } from "lucide-react";
 import { Linkedin } from "lucide-react";
-import { Globe } from "lucide-react";
 import { Code } from "lucide-react";
 
 import { Card, CardContent } from "@/components/ui/card"
@@ -15,7 +12,17 @@ import {
 } from "@/components/ui/carousel"
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
-
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 const CodeFlowLogo = () => (
   <svg viewBox="0 0 100 100" width="50" height="50">
@@ -32,6 +39,9 @@ const CodeFlowLogo = () => (
     </g>
   </svg>
 );
+
+
+
 
 
 function App() {
@@ -81,11 +91,23 @@ function App() {
         <section id="about" className="py-80 bg-gradient-to-b from-black to-blue-900">
           <div className="container mx-auto">
             <h2 className="text-3xl font-bold mb-8 text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-cyan-300">
-              Nossa Visão
+              Sobre mim
             </h2>
             <p className="text-lg text-center max-w-2xl mx-auto text-blue-200">
-              Na CodeFlow, estamos na vanguarda da revolução tecnológica, criando soluções que moldam o futuro digital. 
-              Nossa expertise em desenvolvimento web e mobile transcende o convencional, entregando inovação em cada linha de código.
+              Meu nome é Gabriel Felipe, mais de 3 anos de experiência na área de TI, me dedico a sempre estar buscando conhecimento,
+              minha abordagem combina criatividade técnica com um forte foco na resolução de problemas, permitindo-o enfrentar desafios 
+              complexos e entregar soluções de alta qualidade.
+              
+            </p>
+
+            <p className="text-lg text-center max-w-2xl p-4 mx-auto text-blue-200">
+              Sou formado em Sistemas de Informação e, terminando a Pós-Graduação em desenvolvimento Full-Stack com ênfase em JavaScript.
+            </p>
+
+            <p className="text-lg text-center max-w-2xl mx-auto text-blue-200">
+              
+            Acredito que a tecnologia tem o poder de transformar negócios e melhorar vidas. Minha missão é usar minhas habilidades e conhecimentos 
+            para criar soluções que façam a diferença, sempre buscando aprender e evoluir no processo.
             </p>
           </div>
         </section>
@@ -99,25 +121,91 @@ function App() {
               <Carousel className="flex flex-wrap"> 
                 <CarouselContent>
               {[
-                { title: 'Desenvolvimento Web Avançado', description: "", icon: Code, linkGitHub: "https://github.com/ogabrielfelipe/05-nest-clean" },
-                { title: 'Apps Mobile do Futuro', icon: Globe },
-                { title: 'Inteligência Artificial & IoT', icon: Cpu },
-                { title: 'Inteligência Artificial & IoT', icon: Cpu }
+                
+                  {
+                    title: "FunLearn",
+                    description: "Este projeto tem como objetivo o desenvolvimento de uma ferramenta web, voltada para auxiliar professores e alunos no ensino e aprendizagem de diversos temas, ficando a critério do professor definir o tema e cadastrar as perguntas. O sistema visa trabalhar o conteúdo através de uma interface simples e intuitiva através de um jogo de perguntas e respostas, deixando assim, menos cansativa e mais divertida a aprendizagem.",
+                    image: ["https://live.staticflickr.com/65535/54023233409_86eb8b6052_k.jpg","https://live.staticflickr.com/65535/54022891256_8a60a7bab1_k.jpg","https://live.staticflickr.com/65535/54023344105_a785e89b44_b.jpg","https://live.staticflickr.com/65535/54023233399_c295d4735f_k.jpg", "https://live.staticflickr.com/65535/54023233389_999bae2a90_k.jpg"],
+                    github: "https://github.com/ogabrielfelipe/funLearn",
+                    stack: ["React.js", "Next.js", "Styled Components", "SASS", "Node.js", "Express.js", "Prisma"],
+                  }
+                
               ].map((service, index) => (
+
+               
+                  
+
+
                 <CarouselItem key={index} className="md:basis-2/2 lg:basis-1/3">
                   <div className="p-1">
-                    <Card className="bg-blue-900 bg-opacity-20 p-6 border-transparent rounded-lg shadow-lg">
-                      <CardContent className="flex flex-col gap-3 aspect-square items-center justify-center p-6" >
-                        <service.icon className="w-12 h-12 text-cyan-400 mb-4" />
-                        <h3 className="text-xl font-semibold mb-2 text-blue-300">{service.title}</h3>
-                        <p className="text-blue-200">Transformando ideias em realidade com tecnologias de ponta e soluções inovadoras para o mundo digital em constante evolução.</p>
-                        <button onClick={() => window.open(service.linkGitHub)} className="flex flex-row gap-2 justify-center  bg-gradient-to-r from-blue-500 to-cyan-400 text-white px-8 py-3 rounded-full hover:from-blue-600 hover:to-cyan-500 transition shadow-lg hover:shadow-cyan-500/50">
-                          <GitHubLogoIcon className="w-8 h-8"/>
-                        </button>
-                      </CardContent> 
-                    </Card>
+                    <Dialog>
+                      <Card className="bg-blue-900 bg-opacity-20 border-transparent rounded-lg shadow-lg">
+                        <CardContent className="flex flex-col gap-3 aspect-square items-center justify-center p-6" >
+                            
+                          <h3 className="text-xl font-semibold mb-2 text-blue-300">{service.title}</h3>
+                          <p className="text-blue-200">{service.description}</p>
+                          <div className="flex flex-wrap flex-col gap-4">
+                            <span className="text-blue-200 font-semibold text-l">Tecnologias Utilizadas:</span>
+                            <div className="flex flex-wrap flex-row gap-4">
+                              {service.stack.map((stack, index) => {
+                                return (
+                                  <span key={index} className="text-sm text-blue-50 bg-blue-700 rounded-sm p-2">{stack}</span>
+                                )
+                              })}
+                            </div>
+                            
+                          </div>
+
+                          <DialogContent className="bg-gradient-to-tr from-blue-900 to-blue-400 text-white border-transparent sm:max-w-screen-xl">
+                            
+                                
+                              <DialogHeader>
+                                <DialogTitle>Saiba mais sobre o projeto {service.title}</DialogTitle>
+                              </DialogHeader>
+                              <div className="flex flex-col gap-8 m-10">
+                                <span>Galeria de Fotos:</span>
+                                <Carousel>
+                                  <CarouselContent>
+                                    {service.image.map((image, index) =>(
+                                      <CarouselItem key={index} className="flex justify-center">
+                                        <img src={image} alt={service.title} />
+                                      </CarouselItem>
+                                    ))}
+                                  </CarouselContent>
+
+                                  <CarouselPrevious className=" bg-blue-900 bg-opacity-20" />
+                                  <CarouselNext className=" bg-blue-900 bg-opacity-20"/>
+                                </Carousel>
+                                
+                                <DialogFooter className="sm:justify-center">
+                                  <DialogClose asChild>
+                                    <button>
+                                      <a href={service.github} target="_blank" rel="noopener noreferrer" className="text-blue-50 bg-gradient-to-r from-blue-500 to-cyan-400 text-white px-8 py-3 rounded-full hover:from-blue-600 hover:to-cyan-500 transition shadow-lg hover:shadow-cyan-500/50">
+                                        Acesse o GitHub
+                                      </a>
+                                    </button>
+                                  </DialogClose>
+                                </DialogFooter>
+                                
+                              </div>
+
+                          </DialogContent>
+                        </CardContent> 
+                        <div className="flex justify-center mb-6">
+                          <DialogTrigger asChild className="justify-center">
+                            <button className="text-blue-50 bg-gradient-to-r from-blue-500 to-cyan-400 text-white px-8 py-3 rounded-full hover:from-blue-600 hover:to-cyan-500 transition shadow-lg hover:shadow-cyan-500/50">
+                              Saiba Mais!
+                            </button>
+                          </DialogTrigger>
+                        </div>
+                      </Card>
+
+
+                    </Dialog>
                     </div>
-                </CarouselItem>                
+                </CarouselItem> 
+
+                       
               ))}            
                 </CarouselContent>
 
@@ -136,15 +224,15 @@ function App() {
             <p className="text-lg mb-8 text-blue-200">Estou pronto para te ajudar!</p>
             <div className="flex flex-row gap-2 m-8 justify-center">
               <button className="bg-gradient-to-r from-blue-500 to-cyan-400 text-white px-8 py-3 rounded-full hover:from-blue-600 hover:to-cyan-500 transition shadow-lg hover:shadow-cyan-500/50">
-                <Linkedin />
+                <Linkedin onClick={() => window.open("https://www.linkedin.com/in/ogabrielfelipe/")} className="h-8 w-8"/>
               </button>
 
               <button className="bg-gradient-to-r from-blue-500 to-cyan-400 text-white px-8 py-3 rounded-full hover:from-blue-600 hover:to-cyan-500 transition shadow-lg hover:shadow-cyan-500/50">
-                <Github />
+                <GitHubLogoIcon onClick={() => window.open("https://github.com/ogabrielfelipe")} className="h-8 w-8"/>
               </button>
             </div>
 
-            <span className="text-l flex flex-row gap-2 justify-center">Email: ogabriel.v.felipe@gmail.com</span>
+            <span className="text-l flex flex-row gap-2 justify-center">Email: <a>ogabriel.v.felipe@gmail.com</a></span>
             
           </div>
         </section>
